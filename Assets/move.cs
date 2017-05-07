bd135b1a6fa103ba2c9a5167d857b1bf0f84c2ae
jinwookwak <kwakjinu@gmail.com>
2017-05-07 16:43:37 +0900

    Today I started my project. And I made move the tank and made script for shooting.

---
+++ Assets/move.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0.0f, 5.0f * Time.deltaTime, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-5.0f * Time.deltaTime, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0.0f, -5.0f * Time.deltaTime, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(5.0f * Time.deltaTime, 0.0f, 0.0f);
+        }
+	}
+
+}
