53f2f2a32c80d9c6f356a665bb730ef1f84064d0
soonjeong <woak_uu@naver.com>
2017-05-14 16:49:15 +0900

    Today I made shooting game. 50%

---
+++ Assets/bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour {
+
+    public GameObject player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject != player)
+        {
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+        }
+    }
+}
--- Assets/shotting.cs
+++ Assets/shotting.cs
@@ -2,19 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class shotting : MonoBehaviour {
+public class shotting : MonoBehaviour
+{
+    public GameObject SomPrefab;
+    public float BulletSpeed;
 
 	// Use this for initialization
 	void Start () {
-		
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButton(0))
+		if (Input.GetMouseButtonDown(0))
         {
             Vector3 gameWorldpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
+            GameObject newPrefab = Instantiate(SomPrefab);
+            newPrefab.transform.position = transform.position;
+
+            Vector2 direction = gameWorldpos - transform.position;
+            direction.Normalize();
+            newPrefab.GetComponent<Rigidbody2D>().AddForce(direction * BulletSpeed);
         }
 	}
 }
