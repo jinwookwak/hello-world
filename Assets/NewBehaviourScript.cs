c45c5cb5499730f8712b4d99c96354801bbf2cc7
jy02sung <jy02sung@naver.com>
2017-10-08 19:23:01 +0900

    오늘은 버그수정 다른 조이스틱을 추가했습니다.

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public AudioClip clip;
+
+    // Use this for initialization
+    void Start () {
+        GetComponent<AudioSource>().clip = clip;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        joystick stick = GetComponent<joystick>();
+        if (stick.GetHorizontalValue() != 0.0f ||
+            stick.GetVerticalValue() != 0.0f)
+        {
+            GetComponent<AudioSource>().Play();
+        }
+        else
+        {
+            GetComponent<AudioSource>().Stop();
+        }
+		
+	}
+}
--- Assets/shotting.cs
+++ Assets/shotting.cs
@@ -8,37 +8,40 @@ public class shotting : MonoBehaviour
     public float BulletSpeed;
     public GameObject QPrefab;
     public GameObject startpoint;
+    public GameObject player;
     public float Qcooltime;
     public float Qnowofftime;
     public int maxbulletcount;
+    public float bulletdelay;
+    float delay;
     int bulletcount;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
         bulletcount = maxbulletcount;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if (Input.GetMouseButtonDown(0) && bulletcount >0)
+	void Update ()
+    {
+        joystick stick = GetComponent<joystick>();
+		if ((stick.GetHorizontalValue() != 0.0f ||
+            stick.GetVerticalValue() != 0.0f) &&
+            delay>= bulletdelay)
         {
-            bulletcount -= 1;
             Vector3 gameWorldpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
             GameObject newPrefab = Instantiate(SomPrefab);
             newPrefab.transform.position = startpoint.transform.position;
-            newPrefab.GetComponent<bullet>().player = gameObject;
+            newPrefab.GetComponent<bullet>().player = player;
 
-            Vector2 direction = gameWorldpos - transform.position;
+            Vector2 direction = new Vector2(stick.GetHorizontalValue(), stick.GetVerticalValue());
             direction.Normalize();
             newPrefab.GetComponent<Rigidbody2D>().AddForce(direction * BulletSpeed);
+            delay = 0.0f;
         }
-
-        if (Input.GetKey(KeyCode.E))
-        {
-            bulletcount = maxbulletcount;
-        }
-
+        delay += Time.deltaTime;
         if (Input.GetKey(KeyCode.Q) && Qnowofftime <= 0)
         {
             Qnowofftime = Qcooltime;
